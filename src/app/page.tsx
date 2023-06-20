fab10d252fb91d601b45e2efdb2e631f3a0a71bb
EndYou01 <jorgesenjudo@gmail.com>
2023-06-20 12:35:00 -0400

    Applying useAppSelector and useAppDispatch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,30 @@
+"use client"
+
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { increment, decrement } from "../../redux/features/counterSlice";
 
 export default function HomePage() {
 
+	const count = useAppSelector(state => state.counterReducer.counter)
+	const dispatch = useAppDispatch()
 
 	return (
 		<div>
 			<h1>
-				100
+				total: {count}
 			</h1>
 
-			<button>
+			<button onClick={() => {
+				dispatch(increment())
+			}}>
 				Increment
 			</button>
-			
+
 			<br />
 
-			<button>
+			<button onClick={() => {
+				dispatch(decrement())
+			}}>
 				Decrement
 			</button>
 		</div>
