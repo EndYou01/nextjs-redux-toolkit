fcf705f90a85f722114cb9e78326c68dd0847c6f
EndYou01 <jorgesenjudo@gmail.com>
2023-06-20 13:03:09 -0400

    ending project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,12 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <Providers>
-                    {children}
-                </Providers>
+            <body>
+                <div className="container mx-auto">
+                    <Providers>
+                        {children}
+                    </Providers>
+                </div>
             </body>
         </html>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,54 @@
 
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { increment, decrement } from "../../redux/features/counterSlice";
+import { useGetUserByIdQuery, useGetUsersQuery } from '../../redux/services/userApi'
 
 export default function HomePage() {
 
 	const count = useAppSelector(state => state.counterReducer.counter)
+	const { data, error, isLoading, isFetching } = useGetUsersQuery(null)
 	const dispatch = useAppDispatch()
 
+	if (isLoading || isFetching) return <p>Loading...</p>
+	if (error) return <p>Some Error...</p>
+
 	return (
-		<div>
-			<h1>
+		<>
+			<h1 className="text-center text-2xl">
 				total: {count}
 			</h1>
 
-			<button onClick={() => {
-				dispatch(increment())
-			}}>
-				Increment
-			</button>
+			<div className="flex justify-center gap-x-2"> 
+				<button
+					className="bg-green-500 px-3 py-2 rounded-md m-10"
+					onClick={() => {
+						dispatch(increment())
+					}}>
+					Increment
+				</button>
 
-			<br />
+				<br />
 
-			<button onClick={() => {
-				dispatch(decrement())
-			}}>
-				Decrement
-			</button>
-		</div>
+				<button
+					className="bg-blue-500 px-3 py-2 rounded-md m-10"
+					onClick={() => {
+						dispatch(decrement())
+					}}>
+					Decrement
+				</button>
+			</div>
+
+			<div className="grid grid-cols-3 mx-auto gap-3">
+				{
+					data?.map(user => (
+						<div key={user.id} className="bg-zinc-800 p-4">
+							<p>{user.name}</p>
+							<p>{user.username}</p>
+							<p>{user.email}</p>
+						</div>
+					))
+				}
+			</div>
+		</>
 	)
 }
